065318f89c946fd939350fa06471f920a0ea9b0d
hongyunyan <649330952@qq.com>
2025-10-29 14:47:12 +0800

    Fix: add timedelta import for --days date generation

--- eth_import_transactions.py
+++ eth_import_transactions.py
@@ -3,7 +3,7 @@ import os
 import sys
 import argparse
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 import shutil
 from typing import Optional, List
