aa42f762912c9e2fce2f9126e12c47baeba94422
Yu Jin <yujin.yujin@gmail.com>
2026-05-06 14:40:53 +0800

    Align TiDB primary key with ES document id

--- schema/tidb_eth_transactions.sql
+++ schema/tidb_eth_transactions.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS test.eth_transactions (
   transaction_type BIGINT NULL,
   receipt_effective_gas_price BIGINT NULL,
   random_flag BOOLEAN NOT NULL,
-  PRIMARY KEY (block_timestamp, gas_price, hash)
+  PRIMARY KEY (hash) NONCLUSTERED
 );
 
 CREATE HYBRID INDEX idx_eth_transactions_hybrid
